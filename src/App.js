546d7b2e9f2a57cb17c6449e6820cd267018a192
NaveenMONK <naveenbalaji786@gmail.com>
2022-02-28 11:23:52 +0530

    Comeback push

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import Color from './Color';
+import Values from 'values.js';
+
+function App() {
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values('#F1D00A').all(20));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(20);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+    }
+  };
+  return (
+    <>
+    
+      <section className="container">
+        <h3>color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="your color"
+            className={`${error ? 'error' : null}`}
+          />
+          <button className="btn" type="submit">
+            Change
+          </button>
+        </form>
+      </section>
+      {/* color */}
+      <section className="colors">
+        {list.map((item, index) => {
+          return (
+            <Color key={index} {...item} index={index} hexColor={item.hex} />
+          );
+        })}
+      </section>
+    </>
+
+  );
+}
+
+export default App;
---
+++ src/utils/colorConvertor.js
@@ -0,0 +1,16 @@
+function hexColor(clr) {
+    let hex = clr.toString(16);
+    return hex.length === 1 ? '0' + hex : hex;
+  }
+  
+  
+  function rgbToHex(r, g, b) {
+    return `#${hexColor(r)}${hexColor(g)}${hexColor(b)}`;
+  }
+  
+  export default rgbToHex;
+  
+  
+  /**
+   *  this helper function use to convert rgb to hex colors
+   */
